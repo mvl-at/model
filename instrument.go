6eb38213b9d95b3392b8735b808517d154c0a9dc
Richard Stöckl <richard.stoeckl@aon.at>
2018-10-16 20:28:09 +0200

    added priority to instrument

--- instrument.go
+++ instrument.go
@@ -5,4 +5,5 @@ type Instrument struct {
 	Id         int64  `json:"id"`
 	Name       string `json:"name" roles:"instrument"`
 	NamePlural string `json:"namePlural" roles:"instrument"`
+	Priority   int64  `json:"priority" roles:"instrument"`
 }

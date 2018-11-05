947557611d870d9b31e00049296a176642f7ae25
Richard Stöckl <richard.stoeckl@aon.at>
2018-11-05 14:56:44 +0100

    added end

--- event.go
+++ event.go
@@ -7,6 +7,8 @@ type Event struct {
 	Id            int64     `json:"id"`
 	Date          time.Time `json:"date" roles:"event"`
 	Time          time.Time `json:"time" roles:"event"`
+	End           time.Time `json:"end" roles:"event"`
+	OpenEnd       uint8     `json:"openEnd" roles:"event"`
 	MusicianTime  time.Time `json:"musicianTime" roles:"event"`
 	Name          string    `json:"name" roles:"event"`
 	Note          string    `json:"note" roles:"event"`

67b99097197b9b3ce682ef53801cbd0982a949d8
Richard Stöckl <richard.stoeckl@aon.at>
2018-12-23 22:11:37 +0100

    added subtitles

---
+++ archive.go
@@ -0,0 +1,15 @@
+package model
+
+//defines the archive which stores all scores
+type Archive struct {
+	Id        int64  `json:"id"`
+	Title     string `json:"title" roles:"archive"`
+	Style     string `json:"style" roles:"archive"`
+	Level     string `json:"level" roles:"archive"`
+	Composer  string `json:"composer" roles:"archive"`
+	Arranger  string `json:"arranger" roles:"archive"`
+	Publisher string `json:"publisher" roles:"archive"`
+	Subtitles string `json:"subtitles" roles:"archive"`
+	Location  string `json:"location" roles:"archive"`
+	Note      string `json:"note" roles:"archive"`
+}
